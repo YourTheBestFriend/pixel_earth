de56289ab48dd9700da8e1ef88830b6aec3acd0f
Xhref <goncharovgithub8@gmail.com>
2021-11-09 15:04:46 +0300

    update 09.11.2021 time - 15:04
    update music menu

--- pixel_earth/Assets/Scripts/CameraMove.cs
+++ pixel_earth/Assets/Scripts/CameraMove.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+//using System.Timers;
+using System.Threading;
 
 public class CameraMove : MonoBehaviour
 {
     public GameObject player;
+  
     void Update()
-    {
-        transform.position = new Vector3 (player.transform.position.x, player.transform.position.y+5, -20f);    
+    { 
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, -20f);
     }
 }
